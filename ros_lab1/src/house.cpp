f4d56f08d17db6342113ff657b749d9201f7865b
Ilyalyai <lyapilya@yandex.ru>
2021-11-14 11:51:57 +0300

    Реализовал 3 новых типа ставок

--- ros_lab1/src/house.cpp
+++ ros_lab1/src/house.cpp
@@ -9,41 +9,47 @@ using namespace ros;
 bool add(ros_lab1::gambling_table::Request  &req, ros_lab1::gambling_table::Response &res)
 {
   int user_number = req.number;
-  string user_color = req.color;
+  string user_bet = req.color;
   ROS_INFO("All bets are off!");
   int winning_number = rand()%37;
-  int winning_color_number = rand()%2;
+  string winning_color;
+  if(rand()%2 == 0)
+    winning_color = "black";
+  else if(winning_number == 0)
+    winning_color = "green";
+  else
+    winning_color = "red";
+
   bool is_win = false;
-  switch (user_color[0])
+
+  if(user_number > 36)
   {
-    case 'b':
-      is_win = (winning_number == user_number) && (winning_color_number == 0);
-      break;
-    case 'r':
-      is_win = (winning_number == user_number) && (winning_color_number == 1);
-      break;
-    case 'w':
-      is_win = winning_number == 0;
-      break;
-    default:
-      ROS_INFO("Are you joking with me?!?");
-      ROS_INFO("Of cource you lose, with such a combination!");
-      break;
+    switch (user_number) {
+      case 37:
+        is_win = user_bet.find(winning_color) != std::string::npos;
+        break;
+      case 38:
+        is_win = ((winning_number != 0) &&
+                 (winning_number % 2 == 0 && user_bet.find("even") != std::string::npos) ||
+                 (winning_number % 2 == 1 && user_bet.find("odd") != std::string::npos));
+        break;
+      case 39:
+        is_win = ((winning_number != 0) &&
+                 (winning_number > 18 && user_bet.find("passe") != std::string::npos) ||
+                 (winning_number < 19 && user_bet.find("manque") != std::string::npos));
+        break;
+    }
   }
+
+  else
+    is_win = (winning_number == user_number) && (winning_color.find(user_number));
+
   if (is_win)
     ROS_INFO("Inbelievable! You won!");
   else
-  {
-    string winning_color;
-    if(winning_number == 0)
-      winning_color = "white";
-    else if(winning_color_number == 0)
-      winning_color = "black";
-    else
-      winning_color = "red";
     ROS_INFO("You lose...");
-    ROS_INFO("Winning combination was %s color, %i", winning_color.c_str(), winning_number);
-  }
+
+  ROS_INFO("Winning combination was %s color, %i", winning_color.c_str(), winning_number);
 
   res.result = is_win;
   return true;
--- ros_lab1/src/player.cpp
+++ ros_lab1/src/player.cpp
@@ -6,20 +6,88 @@
 using namespace std;
 using namespace ros;
 
+pair<string, int> calculateInput(int argc, char **argv)
+{
+  pair<string, int> input;
+  string user_bet = argv[1];
+  if(argc == 2)
+  {
+    //Первый случай: ставим чисто на цвет, число тогда вернем 37, потом это поймаем
+    if(user_bet.find("black") != std::string::npos ||
+       user_bet.find("green") != std::string::npos ||
+       user_bet.find("red") != std::string::npos)
+    {
+      input.first = user_bet;
+      input.second = 37;
+    }
+    //Второй- чет/нечет
+    else if(user_bet.find("odd") != std::string::npos ||
+            user_bet.find("even") != std::string::npos)
+    {
+      input.first = user_bet;
+      input.second = 38;
+    }
+    //Третий- от 1 до 18 или от 19 до 36
+    else if(user_bet.find("passe") != std::string::npos ||
+            user_bet.find("manque") != std::string::npos)
+    {
+      input.first = user_bet;
+      input.second = 39;
+    }
+    else
+    {
+      return input;
+    }
+
+  }
+  else if(user_bet.find("black") != std::string::npos ||
+          user_bet.find("red") != std::string::npos ||
+          (user_bet.find("green") != std::string::npos && atoll(argv[2]) == 0))
+  {
+    input.first = user_bet;
+    input.second = atoll(argv[2]);
+  }
+  return input;
+}
+
 int main(int argc, char **argv)
 {
-  if (argc != 3)
+  //3- цвет и номер, 2- чет/нечет, только цвет
+  if (argc != 3 && argc != 2)
   {
     ROS_INFO("usage: player <string>color <uint>number");
+    ROS_INFO("player <string>color <uint>number");
+    ROS_INFO("player <string>color");
+    ROS_INFO("player <string>\"odd/even/passe/manque\"");
     return 1;
   }
 
+  if(argc == 3 && atoll(argv[2]) > 36)
+  {
+    ROS_INFO("Wrong number: 0 <= number <= 36");
+    return 1;
+  }
+
+  pair<string, int> input = calculateInput(argc, argv);
+
+  if(input.first.empty())
+  {
+    ROS_INFO("Sorry, couldn't recognise you're input");
+    return 1;
+  }
+
+  int request_number = input.second;
+  string request_color = input.first;
+
+  //Запуск всех полезных штук, инициализация сервиса для записи значений
   init(argc, argv, "house");
   NodeHandle n;
   ServiceClient client = n.serviceClient<ros_lab1::gambling_table>("gambling_table");
   ros_lab1::gambling_table srv;
-  srv.request.color = argv[1];
-  srv.request.number = atoll(argv[2]);
+
+  srv.request.color = request_color;
+  srv.request.number = request_number;
+
   while(ok())
   {
     if (client.call(srv))
@@ -42,14 +110,30 @@ int main(int argc, char **argv)
       }
       cout << "If you want to play again, input <string>color, enter, <uint>number" << endl;
       cout << "If you want to throw up you're cards, type \"surrender\"" << endl;
-      string new_color;
-      int new_number;
-      cin >> new_color;
-      if(new_color.find("surrender") != std::string::npos)
+      string input;
+      cin >> input;
+
+      if(input.find("surrender") != std::string::npos)
         return 1;
-      cin >> new_number;
-      srv.request.color = new_color;
-      srv.request.number = new_number;
+
+      argv[1] = &input[0];
+
+      cout << "If you want to bet on number, input it, otherwise input \"bet\"" << endl;
+
+      cin >> input;
+
+      if(input.find("bet") != std::string::npos)
+        argc = 2;
+      else
+      {
+        argc = 3;
+        argv[2] = &input[0];
+      }
+
+      pair<string, int> input_pair = calculateInput(argc, argv);
+
+      srv.request.color = input_pair.first;
+      srv.request.number = input_pair.second;
     }
     else
     {
